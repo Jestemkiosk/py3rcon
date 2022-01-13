ab4002145bfd7588e835bb6b1c81e6dc9e351484
Jestemkioskiem <Dlephhi@gmail.com>
2022-01-13 08:41:59 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='py3rcon',  
+
+     version='1.0',
+
+     scripts=['pyrcon'] ,
+
+     author="Wiktor Metryka",
+
+     author_email="jestemkiosk@gmail.com",
+
+     description="API wrapper for RCON.",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="https://github.com/Jestemkioskiem/py3rcon",
+
+     packages=[],
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
